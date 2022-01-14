ba51ae76c3ade7f7b2dcb4fb76852a7e0166b475
Skeletonchampion <greensproblem00@gmail.com>
2022-01-14 19:27:20 +0700

    fix

--- controllers/authLogin.js
+++ controllers/authLogin.js
@@ -24,7 +24,7 @@ export default async function authLogin(req, res) {
             const accessToken = generateAccessToken(myUser);
             const refreshToken = generateRefreshToken(myUser);
 
-            res.cookie("refreshToken", refreshToken { httpOnly: true, sameSite: "none", secure: true });
+            res.cookie("refreshToken", refreshToken, { httpOnly: true, sameSite: "none", secure: true });
 
             res.json({accessToken});
         });
