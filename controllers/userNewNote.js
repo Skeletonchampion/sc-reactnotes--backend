97933ebfc6b6a379959344a1b416390fbd30f119
Speedy <greensproblem00@gmail.com>
2022-02-12 10:03:42 +0700

    fix

--- controllers/userNewNote.js
+++ controllers/userNewNote.js
@@ -3,7 +3,7 @@ import Notes_1 from "../models/Notes_1.js";
 export default async function userNewNote(req, res) {
     try {
         if(req.body.type === "notes_1") {
-            const { id, title, note, label, dateObject, tasks, color, owner } = req.body;
+            const { id, title, note, labels, dateObject, tasks, color, owner } = req.body;
             await Notes_1.create({ id, title, labels, note, date: dateObject, tasks, color, owner });
         }
 
